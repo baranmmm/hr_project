c199cd5f27ffae330c871ac6658c4ee917583580
baranmmm <m.barann84@gmail.com>
2020-11-30 15:08:51 +0000

    update project properties

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -2,11 +2,13 @@ package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
 import com.cybertek.service.ProjectService;
+import com.cybertek.service.RoleService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,16 +18,35 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
-    @Autowired
-    UserService userService;
 
-    @GetMapping("/create")
-    public String projectCreate(Model model){
+        @Autowired
+        RoleService roleService;
 
-        model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("projects", projectService.findAll());
-        model.addAttribute("managers", userService.findAll());
+        @Autowired
+        UserService userService;
 
-        return "/project/create";
-    }
+
+        @GetMapping({"/create"})
+        public String newProject(Model model){
+
+            model.addAttribute("project", new ProjectDTO());
+            model.addAttribute("projects", projectService.findAll());
+            model.addAttribute("managers", userService.findAll());
+
+            return "/project/create";
+        }
+
+        @PostMapping("/create")
+        public String insertUser(ProjectDTO project, Model model){
+
+            projectService.save(project);     //I am saving the user which created in @GetMapping
+
+            return "redirect:/project/create";
+
+        }
+
+
+
+
+
 }
